74e85465626c1fb8d6f41f61eb7a7861316af873
Amy Siu <asiu1028@gmail.com>
2023-08-26 15:55:10 -0400

    iterations

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,88 @@
-function nameMenuItem() {
+//QUESTION: which is more dynamic? both
+// function nameMenuItem(foodName) {
+//   var menuItemName =  `Delicious ${foodName}`
+//   return menuItemName
+// }
 
+//refactors it! simplifies it
+// function nameMenuItem(foodName) {
+//   return `Delicious ${foodName}`
+// }
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`
+}
+
+//this function accepts 3 para
+function createMenuItem(menuItemName, price, type) {
+  //how many parameters?
+  // console.log("price value:", price)
+  // console.log("type value:", type)
+//create a variable and assign it the value of an object
+//has 3 attributes (= 3 key:value)
+  var menuItem = {
+    name: menuItemName,//menuItemName = "Delicious French Toast" bc in test file menuItemName invokes nameMenuItem("French Toast") which has a return value of "Delicious French Toast"
+    //console.log(menuItemName)
+    price: price, //can be "10.99" but to make more dynamic just 10.99
+    //we can test it:
+    type: type //can be "breakfast" but to make more dynamic, just type
+  }
+  return menuItem //return object which contains 3 attributes
+}
+
+function addIngredients(food,ingredients) {
+  //ingredients = [] when you console.log it
+  //food is "cheese"when you console.log(food)
+  //what you want function to do is create an array named ingredients that contains cheese
+   //bc ingredients value is already an empty array in test file
+   if (ingredients.includes(food)) {
+    return ingredients 
+  } else {
+    return ingredients.push(food)
+  }
+}
+//next test: you want to push ["cheese", "peppers", "onion"] into ingredients to make ingredients === to that array.
+//next is only add unique ingredients
+//researched prevent adding duplicates in array: use includes()
+//if ing array does not include(food) then return ingredients.push (push value to string)
+//
+// if (!ingredients.includes(food)) {
+//   return ingredients.puch(food)
+// }
+//if (ingredients.include(food)) return ingredients array without pushing
+
+function formatPrice(price) {
+  //output: should add a dollar sign in front of the price"
+  //price needs to be defined
+ return `$${price}`
+}
+
+function decreasePrice(price) {
+  // console.log("test:",menuItem.price) //can't because out of scope
+  // console.log(price) //=> 6.0
+  return (price -(price * .10))
+  //output we want, is price to decrease by 10%
+}
+
+//think simplicity!
+
+function createRecipe(title, ingredients, type) {
+  
+  var recipe = {
+    title: title, // console.log(title) = delicious eggs & bacon
+    ingredients: ingredients, //  console.log(ingredients) = ["eggs","bacon"]
+    type: type, //console.log(type) = breakfast. Should be same in parameter
+  }
+  return recipe
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
--- test/meal-test.js
+++ test/meal-test.js
@@ -1,7 +1,8 @@
-var chai = require("chai");
-var assert = chai.assert;
+var chai = require("chai"); //using/RQ chai
+var assert = chai.assert;//test chai in the fx
 
-var {
+//imports, what we have as imports reflects exports
+var { //to create these fx, we RQ the meal.js file in src (where code is implemented)
   nameMenuItem,
   createMenuItem,
   addIngredients,
@@ -9,59 +10,84 @@ var {
   decreasePrice,
   createRecipe
 } = require("../src/meal");
+//.. up a level in directory
+//go to src directory
+//then meal file
 
+//the meal,js file, will have a fx called nameMenuItem
+//should be a function that 
 describe("meal.js", function() {
   describe("nameMenuItem", function() {
     it("should be a function", function() {
      assert.isFunction(nameMenuItem);
-    });
+     //asser that it is the function nameMenuItem()
+    });//it is a function, it passes already!
 
-    it.skip("should take in a name and make a delicious name", function () {
+    it("should take in a name and make a delicious name", function () {
       var menuItemName = nameMenuItem("Pizza");
+      //I have a variable declared menuItemName that is assigned to function nameMenuItem('Pizza')
+      //create a variable... or can just return the statement
       assert.equal(menuItemName, "Delicious Pizza");
-    });
+    });  //assert, it wants you to test 2 arguments and see if they are equal
+    //make menuItemName === Delicious Pizza
 
-    it.skip("should be able to create many delicious titles", function () {
+    it("should be able to create many delicious titles", function () {
       var burritoItem = nameMenuItem("Burrito");
       var sushiItem = nameMenuItem("Pizza");
       var tacoItem = nameMenuItem("Taco");
 
       assert.equal(burritoItem, "Delicious Burrito");
+    //should be able to make burritoItem === delicious Burrito
+    // code is already dynamic
       assert.equal(sushiItem, "Delicious Pizza");
       assert.equal(tacoItem, "Delicious Taco");
     });
   });
 
   describe("createMenuItem", function() {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(createMenuItem);
-    });
+    }); //make a function create Menu Item
 
-    it.skip("should create a menu item object", function() {
+    it("should create a menu item object", function() {
       var menuItemName = nameMenuItem("French Toast");
+      //logging it tells us input and output
+      // console.log("menu item name:",menuItemName)
       var menuItem = createMenuItem(menuItemName, 10.99, "breakfast");
-
+      
       assert.equal(menuItem.name, "Delicious French Toast");
+      //make an object of menuItem calling on name (key): to the
+      //returned value of invoking nameMenuItem: "Delicious French toast" 
       assert.equal(menuItem.price, 10.99);
+
+      //object menuItem already made
+      //create key: value of price with value of 10.99
       assert.equal(menuItem.type, "breakfast");
+      //object menuItem already made
+      //create key:value of type: breakfast
+      //
     });
   });
 
   describe("addIngredients", function() {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(addIngredients);
     });
 
-    it.skip("should be able to add ingredients to an array", function() {
-      var ingredients = [];
+    it("should be able to add ingredients to an array", function() {
+      var ingredients = []; // empty array
 
       addIngredients("cheese", ingredients);
-
+      //uses two arguments 
+      //function should add cheese into ingredients
+     
       assert.equal(ingredients.length, 1);
+      //array length should be 1
       assert.deepEqual(ingredients, ["cheese"]);
+      //create ingredients with an array that contains cheese
     });
 
-    it.skip("should be able to add ingredients to an array that already contains ingredients", function() {
+    it("should be able to add ingredients to an array that already contains ingredients", function() {
       var ingredients = [];
 
       addIngredients("cheese", ingredients);
@@ -72,7 +98,7 @@ describe("meal.js", function() {
       assert.deepEqual(ingredients, ["cheese", "peppers", "onion"]);
     });
 
-    it.skip("should only add unique ingredients", function() {
+    it("should only add unique ingredients", function() {
       var ingredients = [];
 
       addIngredients("cheese", ingredients);
@@ -85,23 +111,29 @@ describe("meal.js", function() {
   })
 
   describe("formatPrice", function() {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(formatPrice);
     });
 
-    it.skip("should add a dollar sign in front of the price", function () {
+    it("should add a dollar sign in front of the price", function () {
       var menuItemName = nameMenuItem("French Toast");
+      //using return value "Delicious French Toast" to store in menuItemName variable
       var menuItem = createMenuItem(menuItemName, 10.99, "breakfast");
       var initialPrice = menuItem.price;
-
+      //console.log(initialPrice) => 10.99
+//accessing/getting/setting value of price from menuItem variable and assigning it to initialprice
+    
       var formattedPrice = formatPrice(initialPrice);
       assert.equal(formattedPrice, "$10.99")
-    });
+    }); //the stored return value in the variable: formattedPrice is the same as string "$10.99"
 
-    it.skip("should add a dollar sign in front of a different price", function () {
+    it("should add a dollar sign in front of a different price", function () {
       var menuItemName = nameMenuItem("Carrot Cake");
       var menuItem = createMenuItem(menuItemName, 5.99, "dessert");
+      //know menuItem's return value is an object
       var initialPrice = menuItem.price;
+      
+      //retrieve value of price attribute
 
       var formattedPrice = formatPrice(initialPrice);
       assert.equal(formattedPrice, "$5.99")
@@ -109,42 +141,55 @@ describe("meal.js", function() {
   });
 
   describe("decreasePrice", function() {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(decreasePrice);
     });
 
-    it.skip("should decrease the price by 10%", function() {
+    it("should decrease the price by 10%", function() {
       var menuItemName = nameMenuItem("Fajitas");
       var menuItem = createMenuItem(menuItemName, 6.00, "dessert");
       var decreasedPrice = decreasePrice(menuItem.price);
+//accessing/getting/setting value of price from menuItem variable and assigning it to variable decreasedPrice
+//so decreasedPrice = the assigned value/attribute 6.00
+//can check this by:
+// console.log(menuItem.price) //6.00
+// console.log(decreasedPrice); //undefined bc the decreasePrice fx has nothing in it yet
 
       assert.equal(decreasedPrice, 5.40);
     })
   })
 
   describe("createRecipe", function() {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(createRecipe);
     });
 
-    it.skip("should return a recipe object", function() {
+    it("should return a recipe object", function() {
       var ingredients = [];
-      addIngredients("eggs", ingredients);
-      addIngredients("bacon", ingredients);
+      addIngredients("eggs", ingredients);//invokes addIng fx to add "eggs" in array
+      addIngredients("bacon", ingredients);//invokes addIng fx to add "bacon" in array
 
       var title = nameMenuItem("Eggs & Bacon");
+      // title variable assigned to returned value string Delicious Eggs & Bacon
       var price = formatPrice("10.85")
+      //price variable assigned to returned value of fx (above) string $10.85
       var menuItem = createMenuItem(title, price, "breakfast");
+      //this above same as createMenuItem("Delicious Eggs & Bacon", '$10.85', "breakfast")
+      //menuItem variable assigned to returned value of fx which is an object 
       var menuItemType = menuItem.type;
+      //menuItemType assigned to the type attribute/value 'breakfast' from object menuItem
 
       var recipe = createRecipe(title, ingredients, menuItemType);
+      //aka createRecipe("Delicious Eggs & Bacon", [], 'breakfast')
       assert.equal(recipe.title, "Delicious Eggs & Bacon");
+      //create object var recipe with title(key) === "Delicious Eggs & Bacon"(value)
       assert.deepEqual(recipe.ingredients, ["eggs", "bacon"]);
+      //within object var, make key ingredients same as ["eggs", "bacon"]- done on line 169-170
       assert.equal(recipe.type, "breakfast");
     });
 
 
-    it.skip("should return a different recipe object", function () {
+    it("should return a different recipe object", function () {
       var ingredients = [];
       addIngredients("bread", ingredients);
       addIngredients("cheese", ingredients);
